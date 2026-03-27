e2ffd4b3d6f4fd2234db76f1a014338809612fb1
Tenekes <r000214@gmail.com>
2026-03-27 15:57:18 +0200

    Blog update 3/27/2026 3:57pm: 42 file(s) changed.

--- src/config.ts
+++ src/config.ts
@@ -290,7 +290,7 @@ export const siteConfig: SiteConfig = {
   // Profile Picture
   profilePicture: {
     // [CONFIG:PROFILE_PICTURE_ENABLED]
-    enabled: true, 
+    enabled: false, 
     // [CONFIG:PROFILE_PICTURE_IMAGE]
     image: "/talesorea.png", // Path to your profile image (place in public/ directory)
     // [CONFIG:PROFILE_PICTURE_ALT]
@@ -315,25 +315,19 @@ export const siteConfig: SiteConfig = {
     showMobileMenu: true,
     // [CONFIG:NAVIGATION_PAGES]
     pages: [
-      { title: "Αρθρα", url: "/posts/" },
-      { title: "Projects", url: "/projects/" },
-      { title: "Docs", url: "/docs/" },
-      { title: "Άσχετο", url: "/about/",
-        children: [
-          { title: "Privacy Policy", url: "/privacy-policy/" }
-        ] }
+      { title: "Αρθρα", url: "/posts/" }
     ],
     // [CONFIG:NAVIGATION_SOCIAL]
     social: [
       {
         title: "X",
         url: "https://X.com/talesorea",
-        icon: "X",
+        icon: "x-twitter",
       },
       {
-        title: "New Social",
-        url: "https://example.com",
-        icon: "",
+        title: "Facebook",
+        url: "https://facebook.com/talesorea",
+        icon: "fa-facebook-f",
       },
     ],
   },
@@ -364,13 +358,13 @@ export const siteConfig: SiteConfig = {
     },
     projects: {
       // [CONFIG:HOME_OPTIONS_PROJECTS_ENABLED]
-      enabled: false, // Show featured projects on homepage
+      enabled: true, // Show featured projects on homepage
       // [CONFIG:HOME_OPTIONS_PROJECTS_COUNT]
       count: 2, // Number of projects to show
     },
     docs: {
       // [CONFIG:HOME_OPTIONS_DOCS_ENABLED]
-      enabled: false, // Show featured docs on homepage
+      enabled: true, // Show featured docs on homepage
       // [CONFIG:HOME_OPTIONS_DOCS_COUNT]
       count: 3, // Number of docs to show
     },
